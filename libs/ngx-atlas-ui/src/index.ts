d589893b5293b6979f251d8a76ddbc1faeef07b1
mzkmnk <sma0729mex@gmail.com>
2025-02-28 23:40:18 +0900

    feat: create ngx-atlas-ui library

---
+++ libs/ngx-atlas-ui/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/ngx-atlas-ui/ngx-atlas-ui.component';
---
+++ libs/ngx-atlas-ui/src/lib/ngx-atlas-ui/ngx-atlas-ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ngx-atlas-ui',
+  imports: [CommonModule],
+  templateUrl: './ngx-atlas-ui.component.html',
+  styleUrl: './ngx-atlas-ui.component.css',
+})
+export class NgxAtlasUiComponent {}
