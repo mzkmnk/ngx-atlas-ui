f2c3128faf9ae9103381471d881d8bd0bb1211ac
mzkmnk <sma0729mex@gmail.com>
2025-03-02 11:51:08 +0900

    refactor: add demo components library

---
+++ apps/demo/src/app/components/src/lib/components/components.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-components',
+  imports: [CommonModule],
+  templateUrl: './components.component.html',
+  styleUrl: './components.component.css',
+})
+export class ComponentsComponent {}
